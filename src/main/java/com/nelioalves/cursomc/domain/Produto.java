98c18084d6f642b970222956a7a46779e9072319
rafaeltcavalcanti <rafaeltcavalcanti@gmail.com>
2019-12-20 00:02:05 -0300

    Atualizacao @JsonIgnore

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -15,7 +15,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -38,7 +37,7 @@ public class Produto implements Serializable{
 		inverseJoinColumns = @JoinColumn(name = "categoria_id")
 	)
 	
-	@JsonBackReference
+	@JsonIgnore
 	private List<Categoria> categorias = new ArrayList<>();
 	
 	public Produto() {
